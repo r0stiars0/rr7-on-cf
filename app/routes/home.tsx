49ab1f5f5f51d7df0af6def2f2fce50bd6c3972f
Budi Rostiarso <budi@rostiarso.com>
2026-01-04 00:40:49 +0100

    async-await

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,20 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import React from "react";
+import { Await } from "react-router";
 
-export function meta({}: Route.MetaArgs) {
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function simulatedDelay(value: string, ms: number) {
+  // Simulate a delay between 500ms and 1500ms
+
+  await delay(ms);
+  return value;
+}
+
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
@@ -9,9 +22,18 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export function loader({ context }: Route.LoaderArgs) {
-  return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE };
+  const delayMsRange = [2000, 5000]
+  const ms = Math.trunc(delayMsRange[0] + Math.random() * (delayMsRange[1] - delayMsRange[0]));
+  return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE, ms:ms, slowerMessage: simulatedDelay("This is a longer message",ms) };
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+  return <div><Welcome message={loaderData.message} />
+
+    <React.Suspense fallback={<div className="mx-auto max-w-7xl text-center font-semibold text-gray-100 text-2xl">Loading in {loaderData.ms} ms...</div>}>
+      <Await resolve={loaderData.slowerMessage}>
+        {(value) => <h3 className="mx-auto max-w-7xl text-center font-semibold text-gray-100 text-2xl">Non critical value: {value}</h3>}
+      </Await>
+    </React.Suspense>
+  </div>;
 }
